e0503c097041c9bff286817190efe62cfa37d250
ps-george <g.punter@bboxx.co.uk>
2018-07-13 10:50:12 +0100

    Slack slash command working

--- example/aws/handler.py
+++ example/aws/handler.py
@@ -5,30 +5,30 @@ from six.moves.urllib.parse import parse_qs
 
 def respond(err, res=None):
     return {
-        "statusCode": "400" if err else "200",
-        "text": err.message if err else json.dumps(res),
-        "headers": {
-            "Content-Type": "application/json",
-        }
-    }
+            "statusCode":"400" if err else "200",
+            "body":err.message if err else json.dumps(res),
+            "headers":{"Content-Type": "application/json"},
+            }
 
 
 def execute(text):
     s = None
     err = None
-
     try:
         s = dispatch(text)
     except Exception as error:
         err = error
-
     return respond(err, s)
 
 
 def endpoint(event, context):
-    return respond(None, {'received':str(event)})
-    #params = parse_qs(event["body"])
-    #return respond(None, params["text"])
+    print(event)
+    print(event.get('body'))
+    params = parse_qs(event["body"])
+    print(params)
+    response = respond(None, params["text"])
+    print(response)
+    return response
 
 if __name__=="__main__":
     text = "meow hi"
