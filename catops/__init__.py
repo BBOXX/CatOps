3200bc139cf0bc6ebd105f86833f8ec91bbbaef4
ps-george <g.punter@bboxx.co.uk>
2018-07-19 12:41:30 +0100

    Adding helpers.py file for dealing passing event_text to dispatch

--- catops/__init__.py
+++ catops/__init__.py
@@ -1,3 +1,4 @@
 from .parser import CatParser, ArgumentParserError
 from .dispatcher import Dispatcher, dispatch
+from .helpers import get_text, convert_dispatch, get_slack_colour
 from .slack_handler import SlackHandler
