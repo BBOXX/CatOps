7c563d171eda7cc33dd83ca3faf2d4ea0c13601a
ps-george <g.punter@bboxx.co.uk>
2018-08-10 12:06:15 +0100

    Added verify_request and get_user_slack to catops package

--- catops/__init__.py
+++ catops/__init__.py
@@ -1,3 +1,4 @@
+from .auth import verify_request, get_user_slack
 from .parser import CatParser, ArgumentParserError
 from .dispatcher import Dispatcher, dispatch
 from .helpers import get_text, convert_dispatch, get_slack_colour, create_slack_attachment, create_slack_payload, create_slack_error_payload
